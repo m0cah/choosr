354db5071b2eb4c03db81fac73c023d1133fe140
Michael Neal <mjn@vt.edu>
2024-09-20 18:50:43 -0400

    Changed home screen message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Noah and Jessica :D
         </p>
         <a
           className="App-link"
